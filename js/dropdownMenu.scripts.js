28a5295bcb27fe58b730116adc779d2d44f67e27
Leonardo Tricotti <tricottileo@gmail.com>
2023-11-30 18:25:39 -0300

    commit

--- js/dropdownMenu.scripts.js
+++ js/dropdownMenu.scripts.js
@@ -1,17 +1,19 @@
 // Función que redirige al perfil del usuario
 const goToUserProfile = () => {
-  window.location.href = "https://e-store.up.railway.app/html/userProfile.html";
+  window.location.href =
+    "https://e-store.up.railway.app/front-end/html/userProfile.html";
 };
 
 // Función que redirige al perfil de todos los usuarios
 const goToAllUsersProfile = () => {
-  window.location.href = "https://e-store.up.railway.app/html/allUsers.html";
+  window.location.href =
+    "https://e-store.up.railway.app/front-end/html/allUsers.html";
 };
 
 // Función que redirige a la documentación de la API
 const gotToRealTimeProducts = () => {
   window.location.href =
-    "https://e-store.up.railway.app/html/realTimeProducts.html";
+    "https://e-store.up.railway.app/front-end/html/realTimeProducts.html";
 };
 
 // Función para renderizar el botón de cambio de rol
